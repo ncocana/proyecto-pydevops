287c59a7104dca1399adf7e87dc1f7a5bc250dbb
ncocana <ncocana@cifpfbmoll.eu>
2022-12-02 17:25:31 +0100

    Added create_index and modified create_webpage.py

--- src/create_webpage.py
+++ src/create_webpage.py
@@ -1,7 +1,11 @@
+from create_html.create_index import create_index
 from create_html.create_catalog import create_catalog
 
 #Function to execute all the files needed to create the webpage.
 def create_webpage():
+
+    #Creates the page 'indedx.html'.
+    create_index()
 
     #Creates the page 'catalog.html'.
     create_catalog()
