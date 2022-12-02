c6d72eb06e9e175a8bed7c555b666d211f15ed16
ncocana <ncocana@cifpfbmoll.eu>
2022-12-02 23:49:53 +0100

    Added python file to create 'contact.html'.

--- src/create_webpage.py
+++ src/create_webpage.py
@@ -1,14 +1,18 @@
 from create_html.create_index import create_index
 from create_html.create_catalog import create_catalog
+from create_html.create_contact import create_contact
 
 #Function to execute all the files needed to create the webpage.
 def create_webpage():
 
-    #Creates the page 'indedx.html'.
+    #Creates the page 'index.html'.
     create_index()
 
     #Creates the page 'catalog.html'.
     create_catalog()
+
+    #Creates the page 'contact.html'.
+    create_contact()
 
 #Execute it in the terminal with "python .\src\create_webpage.py".
 create_webpage()
