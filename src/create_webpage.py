f709155cb9e61e8e1b5577eebeb6e86804dc91fa
ncocana <ncocana@cifpfbmoll.eu>
2022-12-02 17:05:27 +0100

    Update: Changed name, deleted previous version, and added the function to create 'catalog.html'.

---
+++ src/create_webpage.py
@@ -0,0 +1,10 @@
+from create_html.create_catalog import create_catalog
+
+#Function to execute all the files needed to create the webpage.
+def create_webpage():
+
+    #Creates the page 'catalog.html'.
+    create_catalog()
+
+#Execute it in the terminal with "python .\src\create_webpage.py".
+create_webpage()
