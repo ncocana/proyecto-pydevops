3ae1a01d2e1809eae906d9923891c5487435f788
ncocana <ncocana@cifpfbmoll.eu>
2022-12-03 19:18:13 +0100

    Add new webpage (booking.html).

--- src/create_webpage.py
+++ src/create_webpage.py
@@ -1,6 +1,7 @@
 from create_html.create_index import create_index
 from create_html.create_catalog import create_catalog
 from create_html.create_contact import create_contact
+from create_html.create_booking import create_booking
 
 #Function to execute all the files needed to create the webpage.
 def create_webpage():
@@ -13,6 +14,9 @@ def create_webpage():
 
     #Creates the page 'contact.html'.
     create_contact()
+    
+    #Creates the page 'booking.html'.
+    create_booking()
 
 #Execute it in the terminal with "python .\src\create_webpage.py".
 create_webpage()
