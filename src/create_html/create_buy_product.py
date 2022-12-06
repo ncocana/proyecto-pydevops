6ad07f0412164bbb856dd6ad763527dc810f8681
ncocana <ncocana@cifpfbmoll.eu>
2022-12-06 20:35:12 +0100

    Fixed gramatical error with page's name.

--- src/create_html/create_buy_product.py
+++ src/create_html/create_buy_product.py
@@ -15,7 +15,7 @@ def create_buy_product():
     html = '''<!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Rental Bike - Buuy a Product</title>
+        <title>Rental Bike - Buy a Product</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
